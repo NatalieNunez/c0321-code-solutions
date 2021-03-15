844a9bd29662e63c5c8e2fd5b6f241fee07d8a76
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-15 11:06:33 -0700

    Completed javascript-primitives-and-variables exercise.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -1,15 +1,19 @@
 var fullName = 'Natalie Nunez';
 var isCool = true;
 var totalPets = 5;
+
 console.log('value of fullName:', fullName);
 console.log('value of isCool:', isCool);
 console.log('value of totalPets:', totalPets);
+
 console.log('typeof fullName:', typeof fullName);
 console.log('typeof isCool:', typeof isCool);
 console.log('type of totalPets:', typeof totalPets);
+
 var unknown = null;
 console.log('value of unknown:', unknown);
 console.log('typeof unknown:', typeof unknown);
+
 var never;
 console.log('value of never:', never);
 console.log('typeof never:', typeof never);

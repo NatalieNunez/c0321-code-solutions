1793d1050e3bfac23a48479de24c876d8c8931c3
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-16 18:31:57 -0700

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-square-of-number.js
@@ -0,0 +1,5 @@
+/* exported getSquareOfNumber */
+function getSquareOfNumber(number) {
+  var numSquared = number ** 2;
+  return numSquared;
+}
---
+++ javascript-functions-practice/get-sum-of-squares.js
@@ -0,0 +1,5 @@
+/* exported getSumOfSquares */
+function getSumOfSquares(x, y) {
+  var squaredSum = x ** 2 + y ** 2;
+  return squaredSum;
+}

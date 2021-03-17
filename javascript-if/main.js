06e9235a78cd98537e99bad839fedc20a5f9939c
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-17 15:07:05 -0700

    Completed javascript-if exercise.

---
+++ javascript-if/main.js
@@ -0,0 +1,81 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  if (person.age === 0 && person.age !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function categorizeAcidity(pH) {
+  if (pH >= 0 && pH <= 6) {
+    return 'acid';
+  }
+  if (pH === 7) {
+    return 'neutral';
+  }
+  if (pH >= 8 && pH <= 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return 'We\'re the warner brothers!';
+  }
+  if (name === 'dot') {
+    return 'I\'m cute~';
+  } else {
+    return 'Goodnight everybody!';
+  }
+}

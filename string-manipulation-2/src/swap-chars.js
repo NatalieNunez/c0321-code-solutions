9a2fb136741234c2588cec753ba02f19fd476633
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-22 10:10:52 -0700

    work on swap-chars

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,8 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-
+  var first = string[secondIndex];
+  var second = string[firstIndex];
+  var firstStr = string.replace(string[firstIndex], first);
+  var swapStr = firstStr.replace(firstStr[secondIndex], second);
+  return swapStr;
 }

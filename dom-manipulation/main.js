270865346a17e2112b463a9fa6dd7b20fb96637a
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-23 10:54:33 -0700

    Fixed if statements

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,20 +6,15 @@ function clickCounter(event) {
   $clickCount.textContent = 'Clicks: ' + ++count;
   if (count < 4) {
     $hotButton.className = 'hot-button cold';
-  }
-  if (count >= 4 && count < 7) {
+  } else if (count >= 4 && count < 7) {
     $hotButton.className = 'hot-button cool';
-  }
-  if (count >= 7 && count < 10) {
+  } else if (count >= 7 && count < 10) {
     $hotButton.className = 'hot-button tepid';
-  }
-  if (count >= 10 && count < 13) {
+  } else if (count >= 10 && count < 13) {
     $hotButton.className = 'hot-button warm';
-  }
-  if (count >= 13 && count < 16) {
+  } else if (count >= 13 && count < 16) {
     $hotButton.className = 'hot-button hot';
-  }
-  if (count >= 16) {
+  } else {
     $hotButton.className = 'hot-button nuclear';
   }
 }

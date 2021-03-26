d824b82e55f7ba0153207c25dad796aa5de32434
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-26 16:44:50 -0700

    Pseudocode planning

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,19 @@
-var $character = document.querySelectorAll('span');
-function handleKeyDown(event) {
+var $phrase = document.querySelector('.phrase');
+// var $character = document.querySelectorAll('span');
 
+function handleKeyDown(event) {
+  // console.log('key down!');
 }
 
-$character.addEventListener('keydown', handleKeyDown);
+$phrase.addEventListener('keydown', handleKeyDown);
+
+// select parent div with class phrase to put an event listener on it
+// for each character, check if the user input is the same as the text content for that span
+//   + if its equal, style it as correct
+//   + if its not equal style it as incorrect
+// find length of characters
+// store number of how many are correct
+// divide length by number correct to get score
+// Send a message to user asking if they want to play again (modal?)
+//   + if yes, start over
+//   + if no, send a message, have a nice day!

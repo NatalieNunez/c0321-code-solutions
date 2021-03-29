dbf3f3952101760c1bf84095d13e3c19445f1ebb
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-28 23:49:49 -0700

    Completed array-manipulation-2 challenge.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,19 +1,13 @@
 /* exported chunk */
-// function chunk(array, size) {
-// var finalArr = [];
-// for (var i = 0; i < size; i++) {
-//  let arr1 = new Array();
-// arr1.push(array[i]);
-// }
-// for (var i = size + 1; i < size; i++) {
-// }
-// }
-
-// parameters: array, number
-// return: array of arrays
-// create an array to hold all the arrays
-// check length of array and divide by size and floor that value
-// if array.length % size !== 0 create one extra array to hold the rest of the data
-
-// condition index equals size
-// then increment initialization
+function chunk(array, size) {
+  var chunkArray = [];
+  for (var i = 0; i < array.length; i++) {
+    var lastChunk = chunkArray[chunkArray.length - 1];
+    if (lastChunk === undefined || lastChunk.length === size) {
+      chunkArray.push([array[i]]);
+    } else {
+      lastChunk.push(array[i]);
+    }
+  }
+  return chunkArray;
+}

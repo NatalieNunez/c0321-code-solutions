ce343684ddd3de9f9a0b412bcbfc99992a75dc2a
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-03 21:57:51 -0700

    work on first algo problem

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,15 @@
+/* exported pick */
+function pick(source, keys) {
+  // var newObject = {};
+  // var sourceArr = Object.keys(source);
+  // for (var i = 0; i < keys.length; i++) {
+  //   if (sourceArr.includes(keys[i]) ) {
+  //     newObject.key[i] =
+  //   }
+  // }
+}
+
+// make a new object to contain the values
+// look at each value in the array and check if its present in source
+// if its present, put that key value pair in the new object
+// return new object

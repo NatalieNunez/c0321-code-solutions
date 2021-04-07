b035b7b0dfe506c4e3df8909b7fecec7a5b1c400
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-07 13:31:48 -0700

    Completed pick, work on omit algo

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,22 @@
 /* exported omit */
+// function omit(source, keys) {
+//   for (var key in source) {
+//     console.log('source object:', source);
+//     console.log('keys array:', keys);
+//     console.log('key:', key);
+//     if (keys.includes(key)) {
+//       console.log('keys includes this key');
+//       delete this.key;
+//       console.log(source);
+//     } else {
+//       console.log('keys doesnt include this key');
+//     }
+//   }
+//   return source;
+// }
+
+// check each property in source
+// if keys includes that property, delete the key value pair
+// return the updated object
+
+// issue with deleting key from source
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,35 +3,11 @@ function pick(source, keys) {
   var newObject = {};
   var sourceArr = Object.entries(source);
   for (var i = 0; i < sourceArr.length; i++) {
-    for (var k = 0; k < 1; k++) {
-      if (keys.includes(sourceArr[i][k])) {
-        newObject.sourceArr[i][k] = sourceArr[i][1];
-      }
+    if (keys.includes(sourceArr[i][0]) && sourceArr[i][1] !== undefined) {
+      var key = sourceArr[i][0];
+      var value = sourceArr[i][1];
+      newObject[key] = value;
     }
   }
-  // for (var i = 0; i < sourceArr.length; i++) {
-  //   if (keys.includes(sourceArr[i][0])) {
-  //     newObject.sourceArr[i][0] = sourceArr[i][1];
-  //   }
-  // }
-  // console.log('source:', source);
-  // console.log('sourceArr:', sourceArr[0][0]);
-  // console.log('keys:', keys);
   return newObject;
 }
-
-// make a new object to contain the values
-// look at each value in the array and check if its present in source
-// if its present, put that key value pair in the new object
-// return new object
-
-// loop through sourceArr
-// if keys includes sourceArr[i][0]
-// newObject.sourceArr[i][0] = sourceArr[i][1]
-
-// for (var i = 0; i < sourceArr.length; i++) {
-//   for (var k = 0; k < 1; k++) {
-//     if (keys.includes(sourceArr[i][k])) {
-//       newObject.sourceArr[i][k] = sourceArr[i][1];
-//  }
-// }

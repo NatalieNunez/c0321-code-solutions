198d588a94e619c7fb288c26fe312aa6a86ad37e
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-08 18:18:30 -0700

    got buttons, arrows, and images to move together, set up timer function

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,10 +1,8 @@
 var $images = document.querySelectorAll('img');
-
 var $previousArrow = document.getElementById('previousImg');
-
 var $nextArrow = document.getElementById('nextImg');
-
-// var $progressDots = document.querySelectorAll('i');
+var $dots = document.querySelectorAll('i');
+var $progressDots = document.querySelector('.progress-dots');
 
 var start = null;
 var currentIndex = null;
@@ -18,50 +16,78 @@ for (var i = 0; i < $images.length; i++) {
   }
 }
 
-function handleClick(event) {
+setInterval(function () {
+  if (currentIndex === $images.length - 1) {
+    $images[currentIndex].classList.add('hidden');
+    $images[0].classList.remove('hidden');
+    $dots[currentIndex].className = 'far fa-circle';
+    $dots[0].className = 'fas fa-circle';
+    currentIndex = 0;
+  } else {
+    $images[currentIndex].classList.add('hidden');
+    $images[currentIndex + 1].classList.remove('hidden');
+    $dots[currentIndex].className = 'far fa-circle';
+    $dots[currentIndex + 1].className = 'fas fa-circle';
+    currentIndex++;
+  }
+}, 3000);
+
+function handleClickArrows(event) {
+  // if (timerId !== false) {
+  //   clearInterval(timerId);
+  //   timerId = false;
+  // } else {
+  //   timer();
+  // }
+
   for (var i = 0; i < $images.length; i++) {
     if ($images[i].classList.length === 0) {
       start = $images[i];
       currentIndex = $images.indexOf(start);
     }
   }
-  // console.log('start:', start);
-  // console.log('index of start:', currentIndex);
 
   if (event.target === $previousArrow) {
     if (currentIndex === 0) {
       $images[currentIndex].classList.add('hidden');
       $images[$images.length - 1].classList.remove('hidden');
+      $dots[currentIndex].className = 'far fa-circle';
+      $dots[$dots.length - 1].className = 'fas fa-circle';
     } else {
       $images[currentIndex].classList.add('hidden');
       $images[currentIndex - 1].classList.remove('hidden');
+      $dots[currentIndex].className = 'far fa-circle';
+      $dots[currentIndex - 1].className = 'fas fa-circle';
     }
   }
   if (event.target === $nextArrow) {
     if (currentIndex === $images.length - 1) {
       $images[currentIndex].classList.add('hidden');
       $images[0].classList.remove('hidden');
+      $dots[currentIndex].className = 'far fa-circle';
+      $dots[0].className = 'fas fa-circle';
     } else {
       $images[currentIndex].classList.add('hidden');
       $images[currentIndex + 1].classList.remove('hidden');
+      $dots[currentIndex].className = 'far fa-circle';
+      $dots[currentIndex + 1].className = 'fas fa-circle';
     }
   }
 }
 
-$previousArrow.addEventListener('click', handleClick);
-$nextArrow.addEventListener('click', handleClick);
+$previousArrow.addEventListener('click', handleClickArrows);
+$nextArrow.addEventListener('click', handleClickArrows);
 
-setInterval(function () {
-  // console.log('3 seconds'); // works
-  // console.log('start:', start); // works
-  // console.log('current index:', currentIndex); // works
-  for (var i = start; i < $images.length; i++) {
-    if ($images[currentIndex] === $images.length - 1) {
-      $images[currentIndex].classList.add('hidden');
-      $images[0].classList.remove('hidden');
+function handleClickDots(event) {
+  for (var i = 0; i < $dots.length; i++) {
+    if ($dots[i] === event.target) {
+      event.target.className = 'fas fa-circle';
+      $images[i].classList.remove('hidden');
     } else {
-      $images[currentIndex].classList.add('hidden');
-      $images[currentIndex + 1].classList.remove('hidden');
+      $dots[i].className = 'far fa-circle';
+      $images[i].classList.add('hidden');
     }
   }
-}, 3 * 1000);
+}
+
+$progressDots.addEventListener('click', handleClickDots);

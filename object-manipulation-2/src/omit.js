8355a3c88fb824c99f69a5757e8fee941b33dcbb
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-11 06:21:14 +0000

    Completed omit problem

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,22 +1,10 @@
 /* exported omit */
-// function omit(source, keys) {
-//   for (var key in source) {
-//     console.log('source object:', source);
-//     console.log('keys array:', keys);
-//     console.log('key:', key);
-//     if (keys.includes(key)) {
-//       console.log('keys includes this key');
-//       delete this.key;
-//       console.log(source);
-//     } else {
-//       console.log('keys doesnt include this key');
-//     }
-//   }
-//   return source;
-// }
-
-// check each property in source
-// if keys includes that property, delete the key value pair
-// return the updated object
-
-// issue with deleting key from source
+function omit(source, keys) {
+  var newObject = {};
+  for (var key in source) {
+    if (!keys.includes(key)) {
+      newObject[key] = source[key];
+    }
+  }
+  return newObject;
+}

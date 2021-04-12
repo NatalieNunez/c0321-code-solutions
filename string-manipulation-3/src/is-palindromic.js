4c4793946ab5364bde2f0ca23cb604a7aadebfc3
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-12 19:03:37 +0000

    work on palindromic problem

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,11 +1,23 @@
 /* exported isPalindromic */
 function isPalindromic(string) {
+  var strSplit = string.split('');
+  var newArr = [];
   var reverseArray = [];
-  for (var i = string.length - 1; i >= 0; i--) {
-    reverseArray.push(string[i]);
+
+  for (var i = 0; i < strSplit.length; i++) {
+    if (strSplit[i] !== ' ') {
+      newArr.push(strSplit[i]);
+    }
   }
-  var newString = reverseArray.join('');
-  if (newString === string) {
+
+  for (var k = newArr.length - 1; k >= 0; k--) {
+    reverseArray.push(newArr[k]);
+  }
+
+  var newString = newArr.join('');
+  var reverseString = reverseArray.join('');
+
+  if (newString === reverseString) {
     return true;
   } else {
     return false;

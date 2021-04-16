d0f78624995da1c48fc57667b9060c88c7a4a199
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-16 06:41:57 +0000

    Completed is-anagram problem

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,26 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var firstArr = firstString.split('');
+  var secondArr = secondString.split('');
+  for (var i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] === ' ') {
+      firstArr.splice(i, 1);
+    }
+  }
+  for (var k = 0; k < secondArr.length; k++) {
+    if (secondArr[k] === ' ') {
+      secondArr.splice(k, 1);
+    }
+  }
+
+  var firstSorted = firstArr.sort();
+  var secondSorted = secondArr.sort();
+  var firstSortedStr = firstSorted.join('');
+  var secondSortedStr = secondSorted.join('');
+
+  if (firstSortedStr === secondSortedStr) {
+    return true;
+  } else {
+    return false;
+  }
+}

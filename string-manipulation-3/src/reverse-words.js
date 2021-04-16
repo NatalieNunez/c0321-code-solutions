071b996df5bcdc1a5e04a62387b51792b9007655
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-16 07:12:10 +0000

    work on reverse problem

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,13 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var array = string.split(' ');
+  // console.log('array:', array);
+  // var reverse = '';
+  for (var i = 0; i < array.length; i++) {
+    for (var k = array[i].length - 1; k >= 0; k--) {
+      // reverse += array[i][k];
+      // console.log(reverse);
+      // console.log(array);
+    }
+  }
+}

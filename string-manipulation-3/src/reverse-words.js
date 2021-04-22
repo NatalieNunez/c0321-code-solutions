1ff510ad1616581a270ae4beceb76838b462e612
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-22 07:37:06 +0000

    solved reverse-words problem

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,12 +1,29 @@
 /* exported reverseWords */
-// function reverseWords(string) {
+function reverseWords(string) {
+  var charArray = string.split('');
+  var reversePhrase = [];
+  for (var i = charArray.length - 1; i >= 0; i--) {
+    reversePhrase.push(charArray[i]);
+  }
+  var newStr = reversePhrase.join('');
+  var wordArray = newStr.split(' ');
+  var reverseWord = [];
+  for (var k = wordArray.length - 1; k >= 0; k--) {
+    reverseWord.push(wordArray[k]);
+  }
+  var finalString = reverseWord.join(' ');
+  return finalString;
+}
 
-// }
-
-// make string into an array of strings
-// check each word and make each word into an array (split)
-// reverse all the characters (reverse)
-// turn it back into a string (join)
-// reassign the new value to the index's value
-// join the result of the array separated by spaces
-// return the result
+// parameter: string
+// return string with characters of words reversed
+// string split character, array of each character
+// create a new array variable
+// split character array, reverse loop through push each into the new array
+// new reversed array, join
+// split new string by spaces
+// create another empty array []
+// reverse loop new array
+// push each index into empty array
+// join that new array
+// return that string

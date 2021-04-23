8d903d57e892a56a150349966dfca897df2f5c93
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-23 01:52:22 +0000

    changed div to an h3 and set a class current to track which index the function should be on

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,20 +1,66 @@
-var $phrase = document.querySelector('.phrase');
-var $character = document.querySelectorAll('span');
+var $currentLetter = document.querySelector('.current');
+var $phrase = document.getElementById('phrase-string');
+var $spans = document.querySelectorAll('span');
+// console.log($spans);
+// console.log($currentLetter);
+// console.log($phrase);
+
+var spanArray = Array.from($spans);
+var currentIndex = spanArray.indexOf($currentLetter);
+// console.log(currentIndex);
 
 function handleKeyDown(event) {
-  // console.log('key down!');
-  // console.log('event.key', event.key);
-  for (var i = 0; i < $character.length; i++) {
-    if ($character[i].textContent === event.key) {
-      $character[i].setAttribute('class', 'true');
+  // debugger;
+  // console.log(event.key);
+  for (var i = currentIndex; i < $spans.length; i++) {
+    if (event.key === $spans[i].textContent) {
+      $spans[i].classList.add('green');
+      $spans[i].classList.remove('current');
+      $spans[i + 1].classList.add('current');
     } else {
-      $character[i].setAttribute('class', 'false');
+      $spans[i].classList.add('red');
     }
-    $character[i + 1].setAttribute('class', 'start');
   }
 }
 
 $phrase.addEventListener('keydown', handleKeyDown);
+// var $phrase = document.querySelector('.phrase');
+// var $character = document.querySelectorAll('span');
+// // console.log($phrase);
+
+// function getCharacters(phrase) {
+//   var charArray = [];
+//   for (var i = 0; i < phrase.length; i++) {
+//     charArray.push(phrase[i].textContent);
+//   }
+//   return charArray;
+// }
+
+// var charactersArr = getCharacters($character);
+
+// function handleKeyDown(event) {
+//   for (var i = 0; i < charactersArr.length; i++) {
+//     if (event.key === charactersArr[i]) {
+//       $character[i].setAttribute('class', 'true');
+//     } else {
+//       $character[i].setAttribute('class', 'false');
+//       i--;
+//     }
+//   }
+// console.log('key down!');
+// console.log('event.key', event.key);
+// for (var i = 0; i < $character.length; i++) {
+// console.log(event.key);
+// if ($character[i].textContent === event.key) {
+//   $character[i].setAttribute('class', 'true');
+// } else {
+//   $character[i].setAttribute('class', 'false');
+// }
+// $character[i + 1].setAttribute('class', 'start');
+// }
+// }
+
+// $phrase.addEventListener('keydown', handleKeyDown);
 
 // select parent div with class phrase to put an event listener on it
 // for each character, check if the user input is the same as the text content for that span

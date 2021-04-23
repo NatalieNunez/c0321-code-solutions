9299c7eb9d6aae0e2b4f3c0b543452a6ade11bd5
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-23 18:49:43 +0000

    got handleKey function to work, working on fixing bug with space key

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,66 +1,37 @@
 var $currentLetter = document.querySelector('.current');
-var $phrase = document.getElementById('phrase-string');
+// var $phrase = document.getElementById('phrase-string');
 var $spans = document.querySelectorAll('span');
-// console.log($spans);
-// console.log($currentLetter);
-// console.log($phrase);
+// console.log('$spans:', $spans);
+// console.log('$currentLetter', $currentLetter);
+// console.log('$phrase', $phrase);
 
 var spanArray = Array.from($spans);
+// console.log('spanArray:', spanArray);
 var currentIndex = spanArray.indexOf($currentLetter);
-// console.log(currentIndex);
+// console.log('currentIndex', currentIndex);
 
 function handleKeyDown(event) {
-  // debugger;
-  // console.log(event.key);
-  for (var i = currentIndex; i < $spans.length; i++) {
-    if (event.key === $spans[i].textContent) {
-      $spans[i].classList.add('green');
-      $spans[i].classList.remove('current');
-      $spans[i + 1].classList.add('current');
-    } else {
-      $spans[i].classList.add('red');
-    }
+  var currentChar = spanArray[currentIndex];
+  if (event.key === currentChar.innerText) {
+    // console.log('true');
+    // // console.log('event.key:', event.key);
+    // console.log('currentChar before:', currentChar);
+    currentChar.classList.add('green');
+    currentChar.classList.remove('red');
+    currentChar.classList.remove('current');
+    currentIndex++;
+    currentChar = spanArray[currentIndex];
+    currentChar.classList.add('current');
+    // console.log('currentChar after:', currentChar);
+  } else {
+    // console.log('false');
+    // console.log('event.key:', event.key);
+    // console.log('currentChar:', currentChar.innerText);
+    currentChar.classList.add('red');
   }
 }
 
-$phrase.addEventListener('keydown', handleKeyDown);
-// var $phrase = document.querySelector('.phrase');
-// var $character = document.querySelectorAll('span');
-// // console.log($phrase);
-
-// function getCharacters(phrase) {
-//   var charArray = [];
-//   for (var i = 0; i < phrase.length; i++) {
-//     charArray.push(phrase[i].textContent);
-//   }
-//   return charArray;
-// }
-
-// var charactersArr = getCharacters($character);
-
-// function handleKeyDown(event) {
-//   for (var i = 0; i < charactersArr.length; i++) {
-//     if (event.key === charactersArr[i]) {
-//       $character[i].setAttribute('class', 'true');
-//     } else {
-//       $character[i].setAttribute('class', 'false');
-//       i--;
-//     }
-//   }
-// console.log('key down!');
-// console.log('event.key', event.key);
-// for (var i = 0; i < $character.length; i++) {
-// console.log(event.key);
-// if ($character[i].textContent === event.key) {
-//   $character[i].setAttribute('class', 'true');
-// } else {
-//   $character[i].setAttribute('class', 'false');
-// }
-// $character[i + 1].setAttribute('class', 'start');
-// }
-// }
-
-// $phrase.addEventListener('keydown', handleKeyDown);
+window.addEventListener('keydown', handleKeyDown);
 
 // select parent div with class phrase to put an event listener on it
 // for each character, check if the user input is the same as the text content for that span

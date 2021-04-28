d08cb919395b8fdb75ad757425e4b19dda45b71f
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-28 21:54:00 +0000

    Moved logic from individual modules to calculate module

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -1,11 +1,20 @@
 const add = require('./add');
-console.log('add:', add);
-
 const subtract = require('./subtract');
-console.log('subtract:', subtract);
-
 const multiply = require('./multiply');
-console.log('multiply:', multiply);
-
 const divide = require('./divide');
-console.log('divide:', divide);
+
+if (process.argv[3] === 'plus') {
+  console.log(`result: ${add.add(process.argv[2], process.argv[4])}`);
+}
+
+if (process.argv[3] === 'minus') {
+  console.log(`return: ${subtract.subtract(process.argv[2], process.argv[4])}`);
+}
+
+if (process.argv[3] === 'times') {
+  console.log(`return: ${multiply.multiply(process.argv[2], process.argv[4])}`);
+}
+
+if (process.argv[3] === 'over') {
+  console.log(`return: ${divide.divide(process.argv[2], process.argv[4])}`);
+}
--- node-require/divide.js
+++ node-require/divide.js
@@ -2,8 +2,4 @@ function divide(num1, num2) {
   return Number(num1) / Number(num2);
 }
 
-if (process.argv[3] === 'over') {
-  console.log(`return: ${divide(process.argv[2], process.argv[4])}`);
-}
-
 module.exports.divide = divide;

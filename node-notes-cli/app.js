561582b03689c31ad377e83424f8fc70345d5968
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-29 22:20:28 +0000

    Completed node-notes-cli challenge

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,47 +1,47 @@
 const fs = require('fs');
-// const jsonObj = require('./data.json');
-// const userInput = process.argv[2];
+const jsonObj = require('./data.json');
+const command = process.argv[2];
+const input2 = process.argv[3];
+const input3 = process.argv[4];
 
-// console.log(typeof JSON.stringify(jsonObj.notes));
-// console.log(typeof jsonObj.notes);
-
-// function read() {
-//   let allNotes = '';
-//   for (const key in jsonObj.notes) {
-//     // console.log(`${key}: ${jsonObj.notes[key]}`);
-//     // allNotes += `${key}: ${jsonObj.notes[key]}`;
-//     allNotes += key + ': ' + jsonObj.notes[key] + '\n';
-//   }
-//   // console.log(allNotes);
-//   return allNotes;
-// }
-
-// if (userInput === 'read') {
-fs.readFile('./data.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+function crudNotes() {
+  if (command === 'read') {
+    let allNotes = '';
+    for (const key in jsonObj.notes) {
+      allNotes += key + ': ' + jsonObj.notes[key] + '\n';
+    }
+    console.log(allNotes);
+  } else if (command === 'create') {
+    jsonObj.notes[jsonObj.nextId] = input2;
+    jsonObj.nextId = jsonObj.nextId + 1;
+    const newNote = JSON.stringify(jsonObj, null, 2);
+    fs.writeFile('data.json', newNote, err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  } else if (command === 'update') {
+    jsonObj.notes[input2] = input3;
+    const updateNote = JSON.stringify(jsonObj, null, 2);
+    fs.writeFile('data.json', updateNote, err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  } else if (command === 'delete') {
+    delete jsonObj.notes[input2];
+    const deleteNote = JSON.stringify(jsonObj, null, 2);
+    fs.writeFile('data.json', deleteNote, err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
   } else {
-    console.log(data);
+    console.log('error: inalid input');
   }
-});
-// }
+}
 
-// user can read their notes
-//   create an fs variable by requiring fs
-//   we'll need access to the data.json file so exporting and importing it
-//     - when requiring json file, its parsed into JS objects
-//   create a variable for user input -> process.argv[2]
-//   use readFile method if user input is read
-//    - parameters: path- data.json | options: utf8 | callback(err, data)
-//      * data parameter will need to be notes object, figure out how to access it
-//      * may need to stringify it since its currently parsed
-//    - console log the data to terminal
-
-// user can add a note
-
-//   conditionals
-//     - if userInput === 'read'
-//     - === 'create'
-//     - === 'update'
-//     - === 'delete'
+crudNotes();

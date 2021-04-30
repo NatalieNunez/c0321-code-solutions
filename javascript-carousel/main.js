11102f1d9646697c70cb924b498dd279228aedd2
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-30 20:21:27 +0000

    utilized updateCarousel function more efficiently to have it update images and dots, simplified click events

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -5,18 +5,17 @@ const $dots = document.querySelectorAll('i');
 const $progressDots = document.querySelector('.progress-dots');
 
 let currentIndex;
+let updateIndex;
 
 function updateCarousel(index) {
-  if (index === $images.length - 1) {
-    $images[index].classList.add('hidden');
-    $images[0].classList.remove('hidden');
-    $dots[index].className = 'far fa-circle';
-    $dots[0].className = 'fas fa-circle';
-  } else {
-    $images[index].classList.add('hidden');
-    $images[index + 1].classList.remove('hidden');
-    $dots[index].className = 'far fa-circle';
-    $dots[index + 1].className = 'fas fa-circle';
+  for (let i = 0; i < $images.length; i++) {
+    if ($images[i] === $images[index]) {
+      $images[i].classList.remove('hidden');
+      $dots[i].className = 'fas fa-circle';
+    } else {
+      $images[i].classList.add('hidden');
+      $dots[i].className = 'far fa-circle';
+    }
   }
 }
 
@@ -26,7 +25,12 @@ setInterval(() => {
       currentIndex = i;
     }
   }
-  updateCarousel(currentIndex);
+  if (currentIndex === $images.length - 1) {
+    updateIndex = 0;
+  } else {
+    updateIndex = currentIndex + 1;
+  }
+  updateCarousel(updateIndex);
 }, 3000);
 
 function handleClick(event) {
@@ -39,39 +43,27 @@ function handleClick(event) {
   if ($progressDots.contains(event.target)) {
     for (let x = 0; x < $dots.length; x++) {
       if (event.target === $dots[x]) {
-        $dots[currentIndex].className = 'far fa-circle';
-        $dots[x].className = 'fas fa-circle';
-        $images[currentIndex].classList.add('hidden');
-        $images[x].classList.remove('hidden');
+        updateCarousel(x);
       }
     }
   }
 
   if (event.target === $previousArrow) {
     if (currentIndex === 0) {
-      $images[currentIndex].classList.add('hidden');
-      $images[$images.length - 1].classList.remove('hidden');
-      $dots[currentIndex].className = 'far fa-circle';
-      $dots[$dots.length - 1].className = 'fas fa-circle';
+      updateIndex = $images.length - 1;
     } else {
-      $images[currentIndex].classList.add('hidden');
-      $images[currentIndex - 1].classList.remove('hidden');
-      $dots[currentIndex].className = 'far fa-circle';
-      $dots[currentIndex - 1].className = 'fas fa-circle';
+      updateIndex = currentIndex - 1;
     }
+    updateCarousel(updateIndex);
   }
+
   if (event.target === $nextArrow) {
     if (currentIndex === $images.length - 1) {
-      $images[currentIndex].classList.add('hidden');
-      $images[0].classList.remove('hidden');
-      $dots[currentIndex].className = 'far fa-circle';
-      $dots[0].className = 'fas fa-circle';
+      updateIndex = 0;
     } else {
-      $images[currentIndex].classList.add('hidden');
-      $images[currentIndex + 1].classList.remove('hidden');
-      $dots[currentIndex].className = 'far fa-circle';
-      $dots[currentIndex + 1].className = 'fas fa-circle';
+      updateIndex = currentIndex + 1;
     }
+    updateCarousel(updateIndex);
   }
 }
 

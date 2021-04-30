269103de5c25022179384fe68efacc10d876a2a8
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-30 01:26:27 +0000

    Removed prototype modification, rewrote setInterval function and click event function

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,60 +1,49 @@
-var $images = document.querySelectorAll('img');
-var $previousArrow = document.getElementById('previousImg');
-var $nextArrow = document.getElementById('nextImg');
-var $dots = document.querySelectorAll('i');
-var $progressDots = document.querySelector('.progress-dots');
+const $images = document.querySelectorAll('img');
+const $previousArrow = document.getElementById('previousImg');
+const $nextArrow = document.getElementById('nextImg');
+const $dots = document.querySelectorAll('i');
+const $progressDots = document.querySelector('.progress-dots');
 
-var start = null; // rethink using
-var currentIndex = null; // rethink using
+let currentIndex;
 
-// NodeList.prototype.indexOf = Array.prototype.indexOf; // dont use this
-
-// this is checking which image has no hidden class and setting to start and currentIndex
-// for (var i = 0; i < $images.length; i++) {
-//   if ($images[i].classList.length === 0) {
-//     start = $images[i];
-//     currentIndex = $images.indexOf(start);
-//   }
-// }
-
-setInterval(function start() {
-  // debugger;
-  // var i = 0;
-  // while ($images[i].classList.value === '') {
-  //   if ($images[i] === $images[$images.length - 1]) {
-  //     $images[i].classList.add('hidden');
-  //     $images[0].classList.remove('hidden');
-  //     $dots[i].className = 'far fa-circle';
-  //     $dots[0].className = 'fas fa-circle';
-  //     i = -1;
-  //   } else {
-  //     $images[i].classList.add('hidden');
-  //     $images[i + 1].classList.remove('hidden');
-  //     $dots[i].className = 'far fa-circle';
-  //     $dots[i + 1].className = 'fas fa-circle';
-  //   }
-  //   i++;
-  // }
-  if (currentIndex === $images.length - 1) {
-    $images[currentIndex].classList.add('hidden');
+function updateCarousel(index) {
+  if (index === $images.length - 1) {
+    $images[index].classList.add('hidden');
     $images[0].classList.remove('hidden');
-    $dots[currentIndex].className = 'far fa-circle';
+    $dots[index].className = 'far fa-circle';
     $dots[0].className = 'fas fa-circle';
-    currentIndex = 0;
   } else {
-    $images[currentIndex].classList.add('hidden');
-    $images[currentIndex + 1].classList.remove('hidden');
-    $dots[currentIndex].className = 'far fa-circle';
-    $dots[currentIndex + 1].className = 'fas fa-circle';
-    currentIndex++;
+    $images[index].classList.add('hidden');
+    $images[index + 1].classList.remove('hidden');
+    $dots[index].className = 'far fa-circle';
+    $dots[index + 1].className = 'fas fa-circle';
   }
+}
+
+setInterval(() => {
+  for (var i = 0; i < $images.length; i++) {
+    if ($images[i].className === '') {
+      currentIndex = i;
+    }
+  }
+  updateCarousel(currentIndex);
 }, 3000);
 
-function handleClickArrows(event) {
-  for (var i = 0; i < $images.length; i++) {
-    if ($images[i].classList.length === 0) {
-      start = $images[i];
-      currentIndex = $images.indexOf(start);
+function handleClick(event) {
+  for (let i = 0; i < $images.length; i++) {
+    if ($images[i].className === '') {
+      currentIndex = i;
+    }
+  }
+
+  if ($progressDots.contains(event.target)) {
+    for (let x = 0; x < $dots.length; x++) {
+      if (event.target === $dots[x]) {
+        $dots[currentIndex].className = 'far fa-circle';
+        $dots[x].className = 'fas fa-circle';
+        $images[currentIndex].classList.add('hidden');
+        $images[x].classList.remove('hidden');
+      }
     }
   }
 
@@ -64,13 +53,11 @@ function handleClickArrows(event) {
       $images[$images.length - 1].classList.remove('hidden');
       $dots[currentIndex].className = 'far fa-circle';
       $dots[$dots.length - 1].className = 'fas fa-circle';
-      currentIndex = $images.length - 1;
     } else {
       $images[currentIndex].classList.add('hidden');
       $images[currentIndex - 1].classList.remove('hidden');
       $dots[currentIndex].className = 'far fa-circle';
       $dots[currentIndex - 1].className = 'fas fa-circle';
-      currentIndex = currentIndex - 1;
     }
   }
   if (event.target === $nextArrow) {
@@ -88,23 +75,6 @@ function handleClickArrows(event) {
   }
 }
 
-$previousArrow.addEventListener('click', handleClickArrows);
-$nextArrow.addEventListener('click', handleClickArrows);
-
-function handleClickDots(event) {
-  for (var i = 0; i < $progressDots.length; i++) {
-    if (event.target === $progressDots[i]) {
-      $images[i].classList.remove('hidden');
-      $dots[i].className = 'far fa-circle';
-
-    }
-  }
-  // $images[currentIndex].classList.add('hidden');
-  // $dots[currentIndex].className = 'far fa-circle';
-  // currentIndex = $dots.indexOf(event.target);
-  // start = $images[currentIndex];
-  // start.classList.remove('hidden');
-  // event.target.className = 'fas fa-circle';
-}
-
-$progressDots.addEventListener('click', handleClickDots);
+$previousArrow.addEventListener('click', handleClick);
+$nextArrow.addEventListener('click', handleClick);
+$progressDots.addEventListener('click', handleClick);

dca2f4d4fea0c478736c9c4f984019456db22445
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-01 08:22:41 +0000

    attempt to solve for special case words

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,20 +1,4 @@
 /* exported titleCase */
-// parameter: string
-// return: string with original title/casing
-// split string by words .split(' ')
-// words three letters or less: lowercase unless its first word
-//   put given words in array and check if the string includes
-//     - if itss the first index, capitalize first letter
-//     - otherwise toLowerCase()
-// Special case words in an array
-// var array = ['JavaScript', 'API'];
-// var str = 'javaScript is cool, apI'; you know what to do ;)
-
-// string that is now an array, nested for loop
-//   if index is a -, capitalize next index
-//   if word is not included in minor words array, capitalize first letter
-//   if word IS included in minor words array, lowercase all letters
-
 var obj = {
   specialCase: [
     'JavaScript',
@@ -24,14 +8,12 @@ var obj = {
 };
 
 function titleCase(title) {
-  // debugger;
   const titleArray = title.split(' ');
   let string = '';
   const arr = [];
   for (let i = 0; i < titleArray.length; i++) {
     if (obj.minorWords.includes(titleArray[i].toLowerCase()) && titleArray[i] !== titleArray[0]) {
       string += titleArray[i].toLowerCase();
-      // string = '';
     } else {
       string += titleArray[i][0].toUpperCase();
     }
@@ -42,17 +24,14 @@ function titleCase(title) {
         string += titleArray[i][x].toLowerCase();
       }
     }
+    for (let z = 0; z < obj.specialCase.length; z++) {
+      if (titleArray[i].toLowerCase() === obj.specialCase[z].toLowerCase()) {
+        string = obj.specialCase[z];
+      }
+    }
     arr.push(string);
     string = '';
   }
   const titleCaseString = arr.join(' ');
   return titleCaseString;
 }
-// console.log('specialCase array:', obj.specialCase);
-// console.log('minorWords array:', obj.minorWords);
-
-// for (var i = 0; i < titleArray.length; i++) {
-//   for (var k = 0; k < titleArray[i].length; k++) {
-//     if ()
-//   }
-// }

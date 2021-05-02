d75da85dbd0f92571719cb7ea88aad42c9b98105
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-02 06:05:25 +0000

    solved cases with a dash in word

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,27 +1,29 @@
 /* exported titleCase */
-var obj = {
-  specialCase: [
-    'JavaScript',
-    'API'
-  ],
-  minorWords: ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to']
-};
-
 function titleCase(title) {
+  var obj = {
+    specialCase: [
+      'JavaScript',
+      'JavaScript:',
+      'API'
+    ],
+    minorWords: ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to']
+  };
   const titleArray = title.split(' ');
   let string = '';
   const arr = [];
   for (let i = 0; i < titleArray.length; i++) {
-    if (obj.minorWords.includes(titleArray[i].toLowerCase()) && titleArray[i] !== titleArray[0]) {
-      string += titleArray[i].toLowerCase();
+    if (obj.minorWords.includes(titleArray[i].toLowerCase()) && titleArray[i]) {
+      string = titleArray[i].toLowerCase();
     } else {
       string += titleArray[i][0].toUpperCase();
     }
     for (let x = 1; x < titleArray[i].length; x++) {
       if (obj.minorWords.includes(titleArray[i].toLowerCase())) {
         string = titleArray[i].toLowerCase();
-      } else {
+      } else if (titleArray[i][x - 1] !== '-') {
         string += titleArray[i][x].toLowerCase();
+      } else {
+        string += titleArray[i][x].toUpperCase();
       }
     }
     for (let z = 0; z < obj.specialCase.length; z++) {

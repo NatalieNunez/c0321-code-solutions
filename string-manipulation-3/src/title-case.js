fa4afa11d659160d5882af38d2c74a5046a940fd
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-03 05:37:21 +0000

    completed title-case problem

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -11,8 +11,12 @@ function titleCase(title) {
   const lowercaseString = title.toLowerCase();
   const lowercaseArr = lowercaseString.split(' ');
   for (let i = 0; i < lowercaseArr.length; i++) {
-    // const lastIndex = lowercaseArr[i - 1];
-    if (!obj.minorWords.includes(lowercaseArr[i]) || lowercaseArr[i - 1] === 'Javascript:') {
+    for (let z = 0; z < obj.specialCase.length; z++) {
+      if (lowercaseArr[i].toLowerCase() === obj.specialCase[z].toLowerCase()) {
+        lowercaseArr[i] = obj.specialCase[z];
+      }
+    }
+    if (!obj.minorWords.includes(lowercaseArr[i]) || lowercaseArr[i - 1] === 'JavaScript:') {
       lowercaseArr[i] = lowercaseArr[i].charAt(0).toUpperCase() + lowercaseArr[i].slice(1);
     }
     for (let x = 0; x < lowercaseArr[i].length; x++) {
@@ -20,42 +24,6 @@ function titleCase(title) {
         lowercaseArr[i] = lowercaseArr[i].slice(0, x) + lowercaseArr[i].charAt(x).toUpperCase() + lowercaseArr[i].slice(x + 1);
       }
     }
-    for (let z = 0; z < obj.specialCase.length; z++) {
-      if (lowercaseArr[i].toLowerCase() === obj.specialCase[z].toLowerCase()) {
-        lowercaseArr[i] = obj.specialCase[z];
-      }
-    }
   }
   return lowercaseArr.join(' ');
-
-  // const titleArray = title.split(' ');
-  // let string = '';
-  // const arr = [];
-  // for (let i = 0; i < titleArray.length; i++) {
-  //   // const lastIndex = titleArray[i - 1].toLowerCase();
-  //   if (obj.minorWords.includes(titleArray[i].toLowerCase())) {
-  //     string = titleArray[i].toLowerCase();
-  //   } else {
-  //     string += titleArray[i][0].toUpperCase();
-  //   }
-  //   for (let x = 1; x < titleArray[i].length; x++) {
-  //     // const lastChar = titleArray[i][x].length - 1;
-  //     if (obj.minorWords.includes(titleArray[i].toLowerCase())) {
-  //       string = titleArray[i].toLowerCase();
-  //     } else if (titleArray[i][x - 1] !== '-') {
-  //       string += titleArray[i][x].toLowerCase();
-  //     } else {
-  //       string += titleArray[i][x].toUpperCase();
-  //     }
-  //   }
-  //   for (let z = 0; z < obj.specialCase.length; z++) {
-  //     if (titleArray[i].toLowerCase() === obj.specialCase[z].toLowerCase()) {
-  //       string = obj.specialCase[z];
-  //     }
-  //   }
-  //   arr.push(string);
-  //   string = '';
-  // }
-  // const titleCaseString = arr.join(' ');
-  // return titleCaseString;
 }

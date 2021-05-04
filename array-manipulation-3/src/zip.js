0703eef95f9b18d01135cf803cfc60b1d7e45e73
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-04 16:34:25 +0000

    completed zip challenge

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,13 @@
 /* exported zip */
+function zip(first, second) {
+  const zipArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] && second[i]) {
+      const arr = [];
+      arr.push(first[i]);
+      arr.push(second[i]);
+      zipArray.push(arr);
+    }
+  }
+  return zipArray;
+}

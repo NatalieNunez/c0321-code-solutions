2ed5ab2252aed6232c014a0e03f74b08bfc9a9c9
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-04 04:48:54 +0000

    completed flatten problem

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,9 +1,14 @@
 /* exported flatten */
 function flatten(array) {
+  const flatArray = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i]) === true) {
-      array[i].join(',');
+      for (let x = 0; x < array[i].length; x++) {
+        flatArray.push(array[i][x]);
+      }
+    } else {
+      flatArray.push(array[i]);
     }
   }
-  return array;
+  return flatArray;
 }

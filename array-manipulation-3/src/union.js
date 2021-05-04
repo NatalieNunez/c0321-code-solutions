6e72661e11100929ba58ea4b08a083d64021acb0
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-04 18:15:28 +0000

    completed union challenge and finished array-manipulation-3 challenge

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,13 @@
 /* exported union */
+function union(first, second) {
+  const unionArray = [];
+  for (let i = 0; i < first.length; i++) {
+    unionArray.push(first[i]);
+  }
+  for (let x = 0; x < second.length; x++) {
+    if (!unionArray.includes(second[x])) {
+      unionArray.push(second[x]);
+    }
+  }
+  return unionArray;
+}

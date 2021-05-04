343a081ee6f7c3f3e188224cef63da215e722456
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-04 19:28:31 +0000

    created object for players

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,27 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// const players = {
+//   player1: {
+//     name: '',
+//     hand: []
+//   },
+//   player2: {
+//     name: '',
+//     hand: []
+//   },
+//   player3: {
+//     name: '',
+//     hand: []
+//   },
+//   player4: {
+//     name: '',
+//     hand: []
+//   }
+// };
+
+// const cardDeck = {
+
+// };
+// _.shuffle(collection) lodash
+// create a function to run game
+//  shuffle card deck using shuffle method
+//  loop through shuffled deck, push two cards into each hand array

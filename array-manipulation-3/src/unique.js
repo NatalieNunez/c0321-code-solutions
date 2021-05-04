9fe77a46a7baeac6a975067ba16a380583a4cfdb
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-04 17:06:35 +0000

    completed unique challenge

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,10 @@
 /* exported unique */
+function unique(array) {
+  const uniqueArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!uniqueArray.includes(array[i])) {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
+}

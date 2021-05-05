03014c5a698a214768372c8f82ab21cec7297a31
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-05 06:01:41 +0000

    tried to make functions for post and delete

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -25,3 +25,27 @@ app.get('/api/notes/:id', (req, res) => {
     res.status(200).json(jsonObj.notes[id]);
   }
 });
+
+// app.post('/api/notes', (req, res) => {
+//   if (!req.body.content) {
+//     res.status(400).json({ error: 'content is a required field' });
+//   // } else if (!success) {
+//   //   res.status(500).json({ error: 'An unexpected error occurred.' });
+//   } else {
+//     jsonObj.notes.id = jsonObj.nextId;
+//     jsonObj.notes.content = req.body;
+//     res.status(201).json(jsonObj);
+//   }
+// });
+
+// app.delete('/api/notes/:id', (req, res) => {
+//   const id = req.params.id;
+//   if (Number(id) !== parseInt(id, 10) || !Number(id) > 0) {
+//     res.status(400).json({ error: 'id must be a positive integer' });
+//   } else if (!jsonObj.notes[req.params.id]) {
+//     res.status(404).json({ error: `cannot find note with the id ${id}` });
+//   } else {
+//     delete jsonObj.notes[id];
+//     res.status(204);
+//   }
+// });

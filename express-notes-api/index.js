8b35ffabbc84b3a875961319431c3a343ff8892e
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-05 01:07:13 +0000

    working on conditionals for get requests

---
+++ express-notes-api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const jsonObj = require('./data.json');
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
+
+app.get('/api/notes', (req, res) => {
+  const notesArray = [];
+  for (const key in jsonObj.notes) {
+    notesArray.push(jsonObj.notes[key]);
+  }
+  res.status(200).json(notesArray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  // console.log('req.params.id', typeof id);
+  if (id !== parseInt(id, 10) || !id > 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (Number(jsonObj.notes[id]) === 'undefined') {
+    res.status(404).json({ error: `cannot find note with the id ${id}` });
+  }
+});

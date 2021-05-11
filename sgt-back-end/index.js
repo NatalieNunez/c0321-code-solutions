e61961ea3a72d448b654df2f1419e10283c7907a
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-11 04:38:45 +0000

    working on put feature

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -44,11 +44,10 @@ app.post('/api/grades', (req, res, next) => {
   const nameValue = req.body.name;
   const courseValue = req.body.course;
   const scoreValue = req.body.score;
-  // console.log(req.body.score);
+
   db.query(sql, values)
     .then(result => {
       if (req.body.score > 100 || req.body.score < 1) {
-        // console.error(err);
         res.status(400).json({
           error: 'Score must be a number between 1 and 100.'
         });
@@ -59,7 +58,6 @@ app.post('/api/grades', (req, res, next) => {
     })
     .catch(err => {
       if (!nameValue || !courseValue || !scoreValue) {
-        // console.error(err);
         res.status(400).json({
           error: 'Name, course, and score are all required contents.'
         });
@@ -71,3 +69,49 @@ app.post('/api/grades', (req, res, next) => {
       }
     });
 });
+
+// app.put('/api/grades/:gradeId', (req, res, next) => {
+//   const gradeId = parseInt(req.params.gradeId, 10);
+//   if (!Number.isInteger(gradeId) || gradeId <= 0) {
+//     res.status(400).json({
+//       error: 'gradeId must be a positive integer.'
+//     });
+//     return;
+//   }
+
+//   const sql = `
+//   update "grades"
+//   set "name" = $1,
+//       "course" = $2,
+//       "score" = $3
+//   where "gradeId" = $4
+//   returning *
+//   `;
+//   const params = [gradeId];
+//   const values = [req.body.name, req.body.course, req.body.score];
+//   const nameValue = req.body.name;
+//   const courseValue = req.body.course;
+//   const scoreValue = req.body.score;
+
+//   db.query(sql, params, values)
+//     .then(result => {
+//       const grade = result.rows[0];
+//       if (!nameValue || !courseValue || !scoreValue) {
+//         res.status(400).json({
+//           error: 'Name, course, and score are all required contents.'
+//         });
+//       } else if (!grade) {
+//         res.status(404).json({
+//           error: `Cannot find grade with "gradeId" ${gradeId}`
+//         });
+//       } else {
+//         res.status(200).json(result.rows[0]);
+//       }
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({
+//         error: 'An Unexpected error occurred.'
+//       });
+//     });
+// });

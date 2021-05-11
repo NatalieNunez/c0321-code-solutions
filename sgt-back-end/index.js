351a5a46e81f638fa515e4a762ab2b184d5f7959
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-11 00:02:50 +0000

    completed post feature

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -33,3 +33,41 @@ app.get('/api/grades', (req, res, next) => {
       });
     });
 });
+
+app.post('/api/grades', (req, res, next) => {
+  const sql = `
+  insert into "grades" ("name", "course", "score")
+  values ($1, $2, $3)
+  returning *
+  `;
+  const values = [req.body.name, req.body.course, req.body.score];
+  const nameValue = req.body.name;
+  const courseValue = req.body.course;
+  const scoreValue = req.body.score;
+  // console.log(req.body.score);
+  db.query(sql, values)
+    .then(result => {
+      if (req.body.score > 100 || req.body.score < 1) {
+        // console.error(err);
+        res.status(400).json({
+          error: 'Score must be a number between 1 and 100.'
+        });
+      } else {
+        res.status(201);
+        res.json(result.rows[0]);
+      }
+    })
+    .catch(err => {
+      if (!nameValue || !courseValue || !scoreValue) {
+        // console.error(err);
+        res.status(400).json({
+          error: 'Name, course, and score are all required contents.'
+        });
+      } else {
+        console.error(err);
+        res.status(500).json({
+          error: 'An Unexpected error occurred.'
+        });
+      }
+    });
+});

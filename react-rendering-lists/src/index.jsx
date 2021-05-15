aa1cd7fc0e5f95cec87a29c9872f7a281a7d7fc6
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-15 07:17:51 +0000

    started pokemonlist function

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function PokemonList(props) {
+//   const pokemon = props.pokemon;
+//   const pokemonListItems = pokemon.map(pokeObj =>
+//     <PokemonListItem key={ pokeObj.number }
+//     name={pokeObj.name} />
+//   );
+//   return (
+//       <ul>
+//       {pokemonListItems}
+//     </ul>
+//   );
+// }
+
+// const pokedex = [
+//   { number: '001', name: 'Bulbasaur' },
+//   { number: '004', name: 'Charmander' },
+//   { number: '007', name: 'Squirtle' },
+//   { number: '025', name: 'Pikachu' },
+//   { number: '039', name: 'Jigglypuff' }
+// ];
+
+// ReactDOM.render(<PokemonList pokedex={pokedex} />, document.getElementById('root'));

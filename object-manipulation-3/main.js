bfef933123d57b7d485c8bb036f631d5b6b2d480
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-16 07:31:17 +0000

    set up function to give each player two cards

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,27 +1,36 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-// const players = {
-//   player1: {
-//     name: '',
-//     hand: []
-//   },
-//   player2: {
-//     name: '',
-//     hand: []
-//   },
-//   player3: {
-//     name: '',
-//     hand: []
-//   },
-//   player4: {
-//     name: '',
-//     hand: []
-//   }
-// };
+function playGame() {
 
-// const cardDeck = {
+  const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
+  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const players = [
+    { name: 'Player1', hand: [], score: 0 },
+    { name: 'Player2', hand: [], score: 0 },
+    { name: 'Player3', hand: [], score: 0 },
+    { name: 'Player4', hand: [], score: 0 }
+  ];
 
-// };
-// _.shuffle(collection) lodash
-// create a function to run game
-//  shuffle card deck using shuffle method
-//  loop through shuffled deck, push two cards into each hand array
+  const cardDeck = () => {
+    const deck = [];
+    for (let i = 0; i < suits.length; i++) {
+      for (let x = 0; x < ranks.length; x++) {
+        const card = { rank: ranks[x], suit: suits[i] };
+        deck.push(card);
+      }
+    }
+    return deck;
+  };
+  const shuffledDeck = _.shuffle(cardDeck());
+
+  for (let i = 0; i < players.length; i++) {
+    if (i > 0) {
+      shuffledDeck.shift();
+      shuffledDeck.shift();
+    }
+    players[i].hand.push(shuffledDeck[0]);
+    players[i].hand.push(shuffledDeck[1]);
+  }
+  console.log(players);
+}
+
+playGame();

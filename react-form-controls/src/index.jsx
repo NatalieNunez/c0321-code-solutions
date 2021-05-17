aeaf9595eccb0f12df1b313e39dfdb22c1090bf2
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-05-17 18:11:13 +0000

    completed react-form-elements exercise

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('state:', this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+        <input type='email' value={this.state.value} onChange={this.handleChange} />
+        <button>Sign Up</button>
+        </label>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.getElementById('root'));

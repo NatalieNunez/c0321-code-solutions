91049ce4427f246827a67dea3324b47e8884614c
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-06-18 04:12:07 +0000

    added an arrowClick method to carousel component

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -6,16 +6,44 @@ class Carousel extends React.Component {
     this.state = {
       index: 0
     };
-    // this.arrowClicks = this.arrowClicks.bind(this);
+    this.arrowClicks = this.arrowClicks.bind(this);
+  }
+
+  arrowClicks(event) {
+    const index = this.state.index;
+    if (event.target.id === 'right') {
+      if (index === this.props.images.length - 1) {
+        this.setState({
+          index: 0
+        });
+      } else {
+        this.setState({
+          index: index + 1
+        });
+      }
+    }
+
+    if (event.target.id === 'left') {
+      if (index === 0) {
+        this.setState({
+          index: this.props.images.length - 1
+        });
+      } else {
+        this.setState({
+          index: index - 1
+        });
+      }
+    }
   }
 
   render() {
     return (
       <>
         <div>
-          <i className="fas fa-chevron-left left-arrow"></i>
-          <img src="https://img.pokemondb.net/sprites/black-white/anim/shiny/zapdos.gif"/>
-          <i className="fas fa-chevron-right right-arrow"></i>
+          <i className="fas fa-chevron-left" id="left" onClick={this.arrowClicks}></i>
+          <img src={this.props.images[this.state.index]} alt="pokemon-image" className="pokemon-image" />
+          {/* <img src="https://img.pokemondb.net/sprites/black-white/anim/shiny/zapdos.gif"/> */}
+          <i className="fas fa-chevron-right" id="right" onClick={this.arrowClicks}></i>
         </div>
         <div></div>
       </>
